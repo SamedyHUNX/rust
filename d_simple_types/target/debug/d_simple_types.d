8dd12ac089b729d166909d5c4c707de312a66779
samedyhunx <samedy.hunx@gmail.com>
2026-01-10 01:12:00 +0700

    primitive types

---
+++ d_simple_types/target/debug/d_simple_types.d
@@ -0,0 +1,1 @@
+/Users/macbookpro/Documents/rust/d_simple_types/target/debug/d_simple_types: /Users/macbookpro/Documents/rust/d_simple_types/src/main.rs
---
+++ d_simple_types/target/debug/deps/d_simple_types-7d853863819d149e.d
@@ -0,0 +1,5 @@
+/Users/macbookpro/Documents/rust/d_simple_types/target/debug/deps/d_simple_types-7d853863819d149e.d: src/main.rs
+
+/Users/macbookpro/Documents/rust/d_simple_types/target/debug/deps/d_simple_types-7d853863819d149e: src/main.rs
+
+src/main.rs:
